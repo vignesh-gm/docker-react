6ffe3f33a41d935c9e500b8cd975208b695fea0e
vgowrimanoharan <VigneshGowriManoharan@quickenloans.com>
2019-07-16 11:17:03 -0400

    Change to home page content

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Realtime change!!
+          Hello World!
         </p>
         <a
           className="App-link"
